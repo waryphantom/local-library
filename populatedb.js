cb6fc1e90af8b3d7f232945de3f62629e088988f
SUDEEP ROY <77157031+waryphantom@users.noreply.github.com>
2021-07-07 23:02:56 +0530

    Update populatedb.js

--- populatedb.js
+++ populatedb.js
@@ -2,7 +2,6 @@
 
 console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
 
-// Get arguments passed on command line
 var userArgs = process.argv.slice(2);
 /*
 if (!userArgs[0].startsWith('mongodb')) {
